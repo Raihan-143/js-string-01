b82ff05348d6a580c436e6866c733af453715d5b
Md. Raihan Hasan Rana <raihan172hasan@gmail.com>
2025-02-14 09:10:01 +0600

    Add files via upload

--- string-decla.js
+++ string-decla.js
@@ -2,3 +2,8 @@ const country='Bangldesh';//declare in single cotation
 const division="Rajshahi";//declare in double cotation
 const district=`Rajshahi`;//declare in backtick sign
 const thana=new String('Tanore');
+console.log(country);
+
+//length & index string
+console.log(district.length);//output-8
+console.log(district[3]);//output-s
