b52e2046b14b59eda3cdc2045601940e9f6c128c
Raihan-143 <raihan172hasan@gmail.com>
2025-02-14 08:59:16 +0600

    added to first string-decla.js file

---
+++ string-decla.js
@@ -0,0 +1,4 @@
+const country='Bangldesh';//declare in single cotation
+const division="Rajshahi";//declare in double cotation
+const district=`Rajshahi`;//declare in backtick sign
+const thana=new String('Tanore');
